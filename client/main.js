97a9d1a64cd58b93a3b150d33cbc490320efb8b6
vchock <vidya.chockalingam92@gmail.com>
2016-10-26 23:06:27 -0700

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,11 +1,9 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import userAuth from '../imports/components/userAuth/userAuth';
-import '../imports/startup/accounts-config.js';
 import 'bootstrap/dist/css/bootstrap.css';
 angular.module('simple-todos', [
   angularMeteor,
-  userAuth.name,
-    'accounts.ui'
+  userAuth.name
 ]);
 
